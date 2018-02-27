732e75cd3a20a68120565f483c3d07909c61b691
lucky20171212 <1656824490@qq.com>
2018-02-27 09:59:10 +0800

    DateTime：2018年2月27日09:58:27 Author：魏璐 Description: 增加了测试内容

--- src/com/javase/lesson/abstractAndInterface/TestAbstract.java
+++ src/com/javase/lesson/abstractAndInterface/TestAbstract.java
@@ -1,5 +1,5 @@
 package com.javase.lesson.abstractAndInterface;
-
+/*
 public class TestAbstract extends AbstractClass{
 
 	@Override
@@ -13,4 +13,31 @@ public class TestAbstract extends AbstractClass{
 		ta.display();
 	}
 
+}*/
+
+public class TestAbstract{
+	public static void main(String[] args) {
+		Chinese ch = new Chinese();
+		ch.speak();
+		
+		Person person = ch;
+		person.speak();
+		ch.display();
+	}
 }
+
+abstract class Person{
+	abstract void speak();
+}
+
+class Chinese extends Person{
+	@Override
+	void speak() {
+		System.out.println("Speak Chinese");
+	}
+	
+	void display(){
+		System.out.println("中国人");
+	}
+}
+
